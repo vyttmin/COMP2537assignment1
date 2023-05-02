8cab1bc5a5a0765a955d7949c873029707991f38
Vyttmin <vinsaq@gmail.com>
2023-05-01 22:13:04 -0700

    fixed keyword require to include

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
-var { database } = require('./databaseConnection');
+var { database } = include('./databaseConnection');
 
 const userCollection = database.db(mongodb_database).collection('users');
 
